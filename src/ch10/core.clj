70b1410e5bed077f6881b0dadcded780f4322f1e
klapnebojsa <klapnebojsa@gmail.com>
2016-03-23 19:27:05 +0100

    .

--- src/ch10/core.clj
+++ src/ch10/core.clj
@@ -35,7 +35,7 @@
    "Listing on page 225."
    (let [program-source
          (slurp (io/reader "examples/reduction.cl"))
-         num-items (Math/pow 2 20)                    ;2 na 20-tu = 1048576
+         num-items (Math/pow 2 10)                    ;2 na 20-tu = 1048576
          bytesize (* num-items Float/BYTES)           ;Float/BYTES = 4    =>   bytesize = 4 * 2na20 = 4 * 1048576 = 4194304.0
          workgroup-size 256
          notifications (chan)
@@ -44,10 +44,25 @@
          ;       (dotimes [n num-items]                ;vrti po n od 0 do 4 * 2na20
          ;         (.putFloat ^java.nio.ByteBuffer d (* n Float/BYTES) 1.0))   ;#object[java.nio.DirectByteBuffer 0x629c81b7 java.nio.DirectByteBuffer[pos=0 lim=4194304 cap=4194304]]
          ;       d)
-         data (let [d (direct-buffer bytesize)]       ;bytesize = 4 * 2na20
-                (dotimes [n num-items]                ;vrti po n od 0 do 4 * 2na20
-                  (.putFloat ^java.nio.ByteBuffer d (* n Float/BYTES) n))   ;#object[java.nio.DirectByteBuffer 0x629c81b7 java.nio.DirectByteBuffer[pos=0 lim=4194304 cap=4194304]]
-                d)         
+         
+         
+         ;data (let [d (direct-buffer bytesize)]       ;bytesize = 4 * 2na20
+         ;       (dotimes [n num-items]                ;vrti po n od 0 do 4 * 2na20
+         ;         (let [p (float (rand-int num-items))]
+         ;           (println p)
+         ;           (.putFloat ^java.nio.ByteBuffer d (* p Float/BYTES) p)))   ;#object[java.nio.DirectByteBuffer 0x629c81b7 java.nio.DirectByteBuffer[pos=0 lim=4194304 cap=4194304]]
+         ;       d)
+         ;data (float-array (rand-int num-items))       
+         ;data (reduce
+         ;      (fn [a b]
+         ;         (conj a (float (rand-int num-items))))
+         ;       [] 
+         ;       (range num-items))               
+         data (float-array (repeatedly num-items #(let [p (rand-int num-items)]
+                                                    (println p)
+                                                    p)))
+           
+         
          cl-partial-sums (* workgroup-size Float/BYTES)       ;4 * 256 = 1024
          partial-output (float-array (/ bytesize workgroup-size))      ;2na20 / 256 = 2na20 / 2na8 = 2na12   - niz od 4096 elemenata
          output (float-array 1)               ;pocetna vrednost jedan clan sa vrednoscu 0.0
@@ -63,14 +78,26 @@
                     profile-event (event)                  ;kreira novi cl_event (dogadjaj)
                     profile-event1 (event)                 ;          -||-
                     profile-event2 (event)]                ;          -||- 
-
-       (println "(apply + (float-array (range 0 1048576))): " (apply + (float-array (range 0 1048576))))
-       (let [r (double  
-                 (loop [sum 0 cnt 1]
-                   (if (= cnt 1048576)
-                           sum
-                           (recur (+ cnt sum) (inc cnt)))))]
-         (println "SUMA: " r))
+       ;(println "buffer contains"
+       ;         (for [x (range num-items)]
+       ;           (.getFloat data x)))
+       
+       (println "(apply + (float-array (range 0" num-items "))): " (apply + (float-array (range 0 num-items))))
+       ;(println (seq data))
+       ;(let [r (double  
+       ;          (loop [sum 0 cnt 1]
+       ;            (if (= cnt 1048576)
+       ;                    sum
+       ;                    (recur (+ cnt sum) (inc cnt)))))]
+       ;(println (for [x (range num-items)]
+       ;             (.get data x)))
+            ;(println "WWWWWWWWWWWWWWW"
+            ;         (for [x (range num-items)]
+            ;           x))
+       
+       ;(let [r (apply + (for [x (range num-items)]
+       ;                   (.getFloat data x)))]         
+       ;(println "KONTROLNA SUMA: " r))
        
        (facts
          
@@ -88,9 +115,11 @@
         (finish! cqueue) => cqueue
         (println "Naive reduction time:"
                  (-> (<!! notifications) :event profiling-info durations :end))
-        (println "output: " (seq output))        
+        (println "output: " (seq output))
+        (println "sta je data: " data)        
+        (apply println "elementi data: " data)
         ;(aget output 0) => num-items
-       (println "============ Scalar reduction ======================================")
+        (println "============ Scalar reduction ======================================")
         ;; ============= Scalar reduction ====================================
          (set-args! reduction-scalar cl-data cl-partial-sums cl-partial-output)  ;setovanje promenjivih u kernelu        reduction-scalar
                                                                                  ;cl-partial-sums=1024  i         
@@ -105,7 +134,7 @@
                  (-> (<!! notifications) :event profiling-info durations :end))
  
         ;(long (first partial-output)) => workgroup-size
-       ;(println "partial-output POJEDINACNA RESENJA: " (seq partial-output))
+       (println "partial-output POJEDINACNA RESENJA: " (seq partial-output))
        (println "UKUPAN ZBIR MEDJUSUMA" (apply + (float-array (seq partial-output))))
         
        (println "============ Vector reduction ======================================")
